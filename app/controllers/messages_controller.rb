4fbe3ba0873cb249464503679292ba074d75d114
Abdurahim .M <Abdugreat2@gmail.com>
2023-08-16 16:52:04 +0300

    Add message controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,10 @@
 class MessagesController < ApplicationController
   def index
+  end
+
+  def random
     @message = Message.order("RANDOM()").first
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    render :json => @message
   end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,17 @@
+import "@hotwired/turbo-rails";
+import "./controllers";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
